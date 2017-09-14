19479f89c619b0469eb86bb15c301f7b68dd5889
zedbay <antoineheurtault78@gmail.com>
2017-09-14 13:12:47 +0200

    d
    d

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php 
-	echo "hello world";
-	 ?>
+<?php require("Codes/RelSchema.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Projet SGBD </title>
+</head>
+<body>
+	<h1>TEST</h1>
+</body>
+</html>
