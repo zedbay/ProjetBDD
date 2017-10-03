a2eb2150fa43c00b955a996487cb206198fb5a7f
zedbay <antoineheurtault78@gmail.com>
2017-10-03 10:33:37 +0200

    s
    s

--- Codes/RelDef.php
+++ Codes/RelDef.php
@@ -5,5 +5,18 @@
 	*/
 	class RelDef {
 		
+		private $_fildeId;
+		private $_relDef;
+
+		public function __construct() {
+
+		}
+
+		public function setFileId() { }
+		public function setRelDef() { }
+
+		public function getFileId() { }
+		public function getRelDef() { }
+		
 	}
 ?>
