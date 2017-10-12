dbc684189ab3964513d8cbf69fd46d58a6cf7985
Stephhawk <31955441+Stephhawks@users.noreply.github.com>
2017-10-12 12:41:38 +0200

    Update RelDef.java

--- ProjetBDDA/src/main/RelDef.java
+++ ProjetBDDA/src/main/RelDef.java
@@ -1,5 +1,23 @@
 package main;
 
 public class RelDef {
+		
+	private int fileId;
+	private relSchema relShema;
+	public RelDef (int fileId,relSchema relShema){
+		this.fileId = fileId;
+		relSchema rel = new relSchema();	
+	}
 
+	public int getFileId() { 
+		return fileId;
+	}
+	
+	public relSchema getRelShema() {
+		return relShema;
+	}
+	
+	public static void main(String[] args) {
+			System.out.println("Stephhawk le BG!");
+		}
 }
