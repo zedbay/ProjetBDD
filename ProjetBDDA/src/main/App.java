d4b2552b98d4b24916186e5a8ae13da67237760b
zedbay <antoineheurtault78@gmail.com>
2017-10-20 11:29:40 +0200

    z
    z

--- ProjetBDDA/src/main/App.java
+++ ProjetBDDA/src/main/App.java
@@ -3,13 +3,12 @@ package main;
 public class App {
 
 	public static void main(String[] args) {
-		PageId pi1 = new PageId(3,3);
-		StringBuffer buff = DiskManager.readPage(pi1);
-		System.out.println(buff);
+		init();
 		//Fenetre fenetre = new Fenetre();
-		//fenetre.setVisible(true);
-		//Choix choice =new Menu(2);
-		//choice.choix(2);
+	}
+	
+	public static void init() {
+		DbDef instanceUniqueDbDef = new DbDef(); 
 	}
 	
 }
