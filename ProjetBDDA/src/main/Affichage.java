a2d7ef2286b157be55e12d5904849521ee468abb
PearToPear <31955626+PearToPear@users.noreply.github.com>
2017-10-26 12:07:07 +0200

    Update Affichage.java

--- ProjetBDDA/src/main/Affichage.java
+++ ProjetBDDA/src/main/Affichage.java
@@ -1,12 +1,12 @@
-package bdd;
+package main;
 
 public interface Affichage {
 	public static void afficher() {
 		System.out.println("BONJOUR\n"
-				+ "Entrez un entier pour exécuter l'une des commandes suivantes :\n\t"
+				+ "Entrez un entier pour exÃ©cuter l'une des commandes suivantes :\n\t"
 				+"\"0\":Sortir\n\t"
-				+ "\"1\": Création d'un fichier\n\t"
-				+ "\"2\":Création d'une page\n\t"
-				+"\"3\":Création dune relation\n\t");
+				+ "\"1\": CrÃ©ation d'un fichier\n\t"
+				+ "\"2\":CrÃ©ation d'une page\n\t"
+				+"\"3\":CrÃ©ation dune relation\n\t");
 	}
 }
