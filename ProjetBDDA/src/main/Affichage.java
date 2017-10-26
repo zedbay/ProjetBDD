5b9d60a5d327797b389c7e4a0cc06e08e608def3
zedbay <antoineheurtault78@gmail.com>
2017-10-26 12:11:11 +0200

    s
    s

--- ProjetBDDA/src/main/Affichage.java
+++ ProjetBDDA/src/main/Affichage.java
@@ -1,12 +1,12 @@
-package bdd;
+package main;
 
 public interface Affichage {
 	public static void afficher() {
 		System.out.println("BONJOUR\n"
-				+ "Entrez un entier pour exécuter l'une des commandes suivantes :\n\t"
+				+ "Entrez un entier pour exï¿½cuter l'une des commandes suivantes :\n\t"
 				+"\"0\":Sortir\n\t"
-				+ "\"1\": Création d'un fichier\n\t"
-				+ "\"2\":Création d'une page\n\t"
-				+"\"3\":Création dune relation\n\t");
+				+ "\"1\": Crï¿½ation d'un fichier\n\t"
+				+ "\"2\":Crï¿½ation d'une page\n\t"
+				+"\"3\":Crï¿½ation dune relation\n\t");
 	}
 }
--- ProjetBDDA/src/main/RelSchema.java
+++ ProjetBDDA/src/main/RelSchema.java
@@ -1,5 +1,17 @@
 package main;
 
+import java.util.ArrayList;
+
 public class RelSchema {
 	
+	private String nom;
+	private int nbrCol;
+	private ArrayList<String> typeCol;
+	private final int stringT = 30;
+	
+	public RelSchema (String nom, int nbrCol, ArrayList<String> typeCol) {
+		this.nom = nom;
+		this.nbrCol = nbrCol;
+		this.typeCol = typeCol;
+	}
 }
