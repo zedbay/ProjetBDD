fd14eb48d8deb90f7acde91a5132a93eeb19562d
zedbay <antoineheurtault78@gmail.com>
2017-10-26 12:43:35 +0200

    s
    s

--- ProjetBDDA/src/main/Affichage.java
+++ ProjetBDDA/src/main/Affichage.java
@@ -7,6 +7,11 @@ public interface Affichage {
 				+"\"0\":Sortir\n\t"
 				+ "\"1\": Cr�ation d'un fichier\n\t"
 				+ "\"2\":Cr�ation d'une page\n\t"
-				+"\"3\":Cr�ation dune relation\n\t");
+				+"\"3\":Cr�ation dune relation\n\t"
+				+ "Entrez un entier pour exécuter l'une des commandes suivantes :\n\t"
+				+"\"0\":Sortir\n\t"
+				+ "\"1\": Création d'un fichier\n\t"
+				+ "\"2\":Création d'une page\n\t"
+				+"\"3\":Création dune relation\n\t");
 	}
 }
