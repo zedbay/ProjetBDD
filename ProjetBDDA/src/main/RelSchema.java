ff90968752225a696abffe90c8a8f8b8f00a11e2
Getsuyobi <31955428+Getsuyobi@users.noreply.github.com>
2017-11-18 17:10:02 +0100

    Changement RelSchema

--- ProjetBDDA/src/main/RelSchema.java
+++ ProjetBDDA/src/main/RelSchema.java
@@ -1,9 +1,12 @@
 package main;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
-public class RelSchema {
+public class RelSchema implements Serializable{
 
+	private static final long serialVersionUID = 1L;
 	private String nom;
 	private int nbrCol;
 	private ArrayList<String> typeCol;
@@ -20,9 +23,35 @@ public class RelSchema {
 				"La relation " + this.nom + " a " + this.nbrCol + " colonnes qui sont : " + this.typeCol.toString());
 		return (rep);
 	}
-	
+
+	public void display() {
+		System.out.println(this.toString());
+	}
 	public int getStringT() {
 		return(stringT);
 	}
+	
+	public String getName() {
+		return nom;
+	}
 
+	public void setName(String nom) {
+		this.nom = nom;
+	}
+
+	public int getNbrCol() {
+		return nbrCol;
+	}
+
+	public void setNbrCol(int nbrCol) {
+		this.nbrCol = nbrCol;
+	}
+
+	public List<String> getTypeCol() {
+		return typeCol;
+	}
+
+	public void setTypeCol(ArrayList<String> typeCol) {
+		this.typeCol = typeCol;
+	}
 }
